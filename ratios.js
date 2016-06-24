30e84536258dad62cf1e57145d66c3fdae819cdb
Jason Marmon <jtmarmon@gmail.com>
2016-06-23 21:48:16 -0400

    initial commit

---
+++ ratios.js
@@ -0,0 +1,21 @@
+const nodes = document.querySelectorAll('tr[class="total"] td');
+const [_, totalCals, carbs, fat, protein] = Array.prototype.map.call(nodes, ele => {
+    const raw = ele.innerHTML;
+    return parseInt(raw.replace(/,/g, ''));
+});
+
+const round = v => Math.round(v * 100) / 100;
+const calcRatio = v => round(v * 100 / totalCals);
+
+const htmlString = `
+    <tr class="total ratios">
+        <td class="first"> Calorie % </td>
+        <td> - </td>
+        <td> ${calcRatio(carbs * 4)}% </td>
+        <td> ${calcRatio(fat * 9)}% </td>
+        <td> ${calcRatio(protein * 9)}% </td>
+        <td> - </td>
+        <td> - </td>
+    </tr>
+`;
+document.querySelectorAll('.table0 .total.remaining')[0].insertAdjacentHTML('afterend', htmlString);
